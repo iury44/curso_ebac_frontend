7a6838e63b88b863ed2d27e4abc51fa62bfb36cd
Iury <iuryhenrique44@gmail.com>
2024-05-20 08:21:19 -0300

    Atualização do js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,36 @@
-import * as elementos from './elementos.js'
-document.addEventListener('DOMContentLoaded',()=>{
-  Api()
-})
+import * as elementos from './elementos.js';
 
-
-
+document.addEventListener('DOMContentLoaded', () => {
+  Api();
+});
 
 const repositorio = document.getElementById('count-repositorio');
 const seguidores = document.getElementById('count-seguidores');
 const seguindo = document.getElementById('count-seguindo');
 
-const profileAvatar = document.getElementById('profile__avatar')
-const profileName = document.getElementById('profile__name')
-const profileUsername = document.getElementById('profile__username')
+const profileAvatar = document.getElementById('profile__avatar');
+const profileName = document.getElementById('profile__name');
+const profileUsername = document.getElementById('profile__username');
 
-function Api() {  
-  const endpoint = `https://api.github.com/users/iury44`;
+async function Api() {
+  const endpoint = 'https://api.github.com/users/iury44';
   console.log('Endpoint:', endpoint);
 
-  fetch(endpoint)
-    .then(response => {
-      return response.json();
-    })
-    .then(data =>{
-       const {login,name,public_repos,followers,following,avatar_url} = data
-      profileName.innerHTML = name
-      profileUsername.innerHTML = login
-      profile__avatar.src = avatar_url
-      repositorio.innerText = public_repos;
-      seguidores.innerText = followers.toString();
-      seguindo.innerText = following.toString();
-      
-      // console.log(login,name,public_repos,followers,following)
-    })
-    .catch(error => {
-      console.error('Erro ao buscar dados:', error);
-    });
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    const { login, name, public_repos, followers, following, avatar_url } = data;
+    
+    profileName.innerHTML = name;
+    profileUsername.innerHTML = login;
+    profileAvatar.src = avatar_url;
+    repositorio.innerText = public_repos;
+    seguidores.innerText = followers.toString();
+    seguindo.innerText = following.toString();
+  } catch (error) {
+    console.error('Erro ao buscar dados:', error);
+  }
 }
